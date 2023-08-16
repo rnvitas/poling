921407a99bd0a4f0f1b59bca584c6c37ce7b47b4
rnvitas <85440769+rnvitas@users.noreply.github.com>
2023-08-16 14:36:32 +0700

    Delete application directory

--- application/views/webview/_parts/wrapper.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
-$this->load->view('webview/_parts/head');
-// $this->load->view('_parts/top_nav');
-// $this->load->view('_parts/side_nav_db');
-$this->load->view($content);
-// $this->load->view('layouts/_parts/nav_bottom');
-// $this->load->view('_parts/footer');
-// $this->load->view('_parts/js');
-if (isset($content_js)) $this->load->view($content_js);
